c38743bace115bdba8fbfbac683cde798c1cd064
m0000g <m0000g@gmail.com>
2022-09-07 10:26:24 -0600

    working on data types assigments

---
+++ assigments/02_data_types/02_data_types.js
@@ -0,0 +1,11 @@
+let country = "Costa Rica";
+let continent = "Central America";
+let population = "5,094,114";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIland = false;
+let language;
+console.log(typeof isIland, typeof population, typeof country, typeof language)
