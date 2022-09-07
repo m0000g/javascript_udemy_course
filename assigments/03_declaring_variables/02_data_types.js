ec458d8f5a1f927695b0c82442c40484a7033ce2
m0000g <m0000g@gmail.com>
2022-09-07 10:32:56 -0600

    declaring variables

---
+++ assigments/03_declaring_variables/02_data_types.js
@@ -0,0 +1,13 @@
+const country = "Costa Rica";
+const continent = "Central America";
+const population = "5,094,114";
+const isIland = false;
+const language = "Spanish";
+const idioma = "English";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(language);
+
+console.log(typeof isIland, typeof population, typeof country, typeof language);
