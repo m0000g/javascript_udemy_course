562da438db56a49d1de90205a781efc42ac7e683
m0000g <m0000g@gmail.com>
2022-09-16 11:08:54 -0600

    Temporary challenge #3 solution

--- code_challenges/part_2/03_codding_challenge/script.js
+++ code_challenges/part_2/03_codding_challenge/script.js
@@ -13,6 +13,7 @@ method on both objects). Store the BMI value to a property, and also return it
 from the method
 3. Log to the console who has the higher BMI, together with the full name and the 
 respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+
 Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m 
 tall.
 */
@@ -20,5 +21,22 @@ tall.
 // Formula: BMI = mass / height ** 2
 // full name, mass, and height
 
-const mark = [`Mark Miller`, 78, 1.69];
-const john = [`John Smith`, 98, 1.95];
+const bmiCalculate = function (mass, height) {
+    return mass / height ** 2;
+}
+
+const mark = [`Mark Miller`, 78, 1.69, bmiCalculate(78, 1.69)];
+const john = [`John Smith`, 98, 1.95, bmiCalculate(98, 1.95)];
+
+console.log(mark);
+console.log(john);
+
+if (mark[mark.length - 1] > john[john.length - 1]) {
+    console.log(`Mark's BMI (${mark[mark.length - 1]}) is higher than Jhon's (${john[john.length - 1]})!)`);
+}
+else if (john[john.length - 1] > mark[mark.length - 1]) {
+    console.log(`Jhon's BMI (${john[john.length - 1]}) is higher than Marks's (${mark[mark.length - 1]})!)`);
+}
+else {
+    console.log(`Mark and Jhon have the same BMI of ${mark[mark.length - 1]}`)
+}
