60dbf6fcceedda70e3cadab735b13300c123dc8c
m0000g <m0000g@gmail.com>
2022-09-27 11:55:34 -0600

    Assigments intro to objects and dot vs Bracket notation

---
+++ assigments/18_intro_to_objects/script.js
@@ -0,0 +1,17 @@
+/* LECTURE: Introduction to Objects
+1. Create an object called 'myCountry' for a country of your choice, containing 
+properties 'country', 'capital', 'language', 'population' and 
+'neighbours' (an array like we used in previous assignments
+ */
+
+'use strict';
+
+const myCountry = {
+    contry: `Costa Rica`,
+    capital: `San José`,
+    language: `Spanish`,
+    population: 5,
+    neighbours: [`Nicaragua, Panamá`]
+}
+
+console.log(myCountry);
