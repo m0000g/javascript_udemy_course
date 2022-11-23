d13d14d160d3545cdd15108d8af72617d4232021
m0000g <m0000g@gmail.com>
2022-11-22 18:39:06 -0600

    Teacher's feedback and Bonus 4 cleaner

--- code_challenges/06_Data_structures_modern_operators_challenge_2/script.js
+++ code_challenges/06_Data_structures_modern_operators_challenge_2/script.js
@@ -44,30 +44,46 @@ const game = {
 // 1.- Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
 
 for (const [goal, player] of game.scored.entries()) {
-  console.log(`Goal ${goal}: ${player}`);
+  console.log(`Goal ${goal + 1}: ${player}`);
 }
 
 // 2.- Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember) - Do not destruct the object into variables.
 // Calculate the average of the elements in the object.
 
+// My solution
 let average = 0;
 for (const value of Object.values(game.odds)) {
   average += value;
 }
 console.log(average / Object.keys(game.odds).length);
 
+// Teacher's solution
+/* const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average); */
+
 // 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
 // Odd of victory Bayern Munich: 1.33
 // Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
 // Get the team names directly from the game object, don't hardcode them  (except for "draw").
 // Hint: Note how the odds and the game objects have the same property names
 
+// My solution
 for (const [key, value] of Object.entries(game.odds)) {
   console.log(
     `Odd of ${game[key] ? 'victory ' : 'draw'}${game[key] || ''}: ${value}`
   );
 }
 
+// Teacher's solution
+/* for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
+ */
 // 4. Bonus: Create an object called 'scorers' which contains the names of the
 // players who scored as properties, and the number of goals as the value. In this
 // game, it will look like this:
@@ -78,7 +94,8 @@ for (const [key, value] of Object.entries(game.odds)) {
 // }
 
 let scorers = {};
-for (const [key, player] of Object.entries(game.scored)) {
-  scorers[player] = scorers[player] ? scorers[player] + 1 : 1;
+for (const name of game.scored) {
+  // scorers[name] = scorers[name] ? scorers[name] + 1 : 1;
+  name in scorers ? (scorers[name] += 1) : (scorers[name] = 1);
 }
 console.log(scorers);
