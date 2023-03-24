00a7ea041fc3b495476c8efe8029a7c6c82948b6
MG <m0000g@gmail.com>
2023-03-24 17:30:40 -0600

    Teacher's solution

--- code_challenges/06_Data_structures_modern_operators_challenge_2/script.js
+++ code_challenges/06_Data_structures_modern_operators_challenge_2/script.js
@@ -71,19 +71,19 @@ console.log(average); */
 // Get the team names directly from the game object, don't hardcode them  (except for "draw").
 // Hint: Note how the odds and the game objects have the same property names
 
-// My solution
-for (const [key, value] of Object.entries(game.odds)) {
+// My solution a bit crazy, not really good
+/* for (const [key, value] of Object.entries(game.odds)) {
   console.log(
     `Odd of ${game[key] ? 'victory ' : 'draw'}${game[key] || ''}: ${value}`
   );
-}
+} */
 
 // Teacher's solution
-/* for (const [team, odd] of Object.entries(game.odds)) {
+for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
- */
+
 // 4. Bonus: Create an object called 'scorers' which contains the names of the
 // players who scored as properties, and the number of goals as the value. In this
 // game, it will look like this:
