2c51b1e877ed51dbe9f4978e84fcf4fd3c8d1269
MG <m0000g@gmail.com>
2023-03-24 21:09:46 -0600

    Challenge 3, Datastructures

--- code_challenges/07_Data_structures_modern_operators_challenge_3/script.js
+++ code_challenges/07_Data_structures_modern_operators_challenge_3/script.js
@@ -31,3 +31,40 @@ const gameEvents = new Map([
   [80, '⚽ GOAL'],
   [92, '🟡️ Yellow card'],
 ]);
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+const events = [...new Set(gameEvents.values())];
+console.log(`Events ${events}`);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
+console.log(`gameEvents without minute 64: `);
+console.log(gameEvents);
+
+// 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+const newArray = new Array(...gameEvents.keys());
+console.log(`New Array, only keys: `);
+console.log(newArray);
+const initialvalue = 0;
+const difference = newArray.map((value, i, arr) => arr[i + 1] - arr[i]);
+difference.pop(difference.length);
+const sum = difference.reduce((sum, number) => {
+  return sum + number;
+}, 0);
+console.log(sum);
+console.log(
+  `An event happened, on average, every ${Math.round(
+    sum / difference.length
+  )}  minutes`
+);
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽ GOAL
+let half = '';
+for (const [key, value] of gameEvents) {
+  if (key < 45) {
+    half = 'FIRST';
+  } else {
+    half = 'SECOND';
+  }
+  console.log(`[${half} HALF] ${key}: ${value}`);
+}
