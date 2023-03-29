f06b2b4410246a93af528245462cee12c9f49a78
MG <m0000g@gmail.com>
2023-03-29 15:44:29 -0600

    Simplification using join in strings

--- code_challenges/09_Closer_look_functions_challenge_1/script.js
+++ code_challenges/09_Closer_look_functions_challenge_1/script.js
@@ -4,9 +4,7 @@
 Coding Challenge #1
 Let's build a simple poll app!
 
-A poll has a question, an array of options from which people can choose, and an
-array with the number of replies for each option. This data is stored in the starter
-'poll' object below.
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below.
 
 Your tasks:
 1. Create a method called 'registerNewAnswer' on the 'poll' object. The
@@ -25,25 +23,18 @@ What is your favourite programming language?
 1.2.
 
 Based on the input number, update the 'answers' array property. For
-example, if the option is 3, increase the value at position 3 of the array by
-
-1. Make sure to check if the input is a number and if the number makes
+example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes
 sense (e.g. answer 52 wouldn't make sense, right?)
 
 2. Call this method whenever the user clicks the "Answer poll" button.
 
 3. Create a method 'displayResults' which displays the poll results. The
-method takes a string as an input (called 'type'), which can be either 'string'
-or 'array'. If type is 'array', simply display the results array as it is, using
-console.log(). This should be the default option. If type is 'string', display a
-string like "Poll results are 13, 2, 4, 1".
+method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 
 4. Run the 'displayResults' method at the end of each
 'registerNewAnswer' method call.
 
-5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
-data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
-object! So what should the this keyword look like in this situation? 
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation? 
 
 Test data for bonus:
 Data 1: [5, 2, 3]
@@ -56,4 +47,54 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    let message = `${this.question}\n${this.options.join(
+      '\n'
+    )} \n (Write option number)`;
+
+    const answer = prompt(`${message ? message : undefined}`);
+    if (answer) {
+      const answerNumber = Number(answer);
+      if (typeof answerNumber == 'number') {
+        if (answerNumber >= 0 && answerNumber <= 3) {
+          console.log('Number allowed');
+          this.answers[answerNumber]++;
+        } else {
+          console.log('Number not allowed');
+        }
+      } else {
+        console.log('Please, insert a number');
+      }
+    }
+    this.displayResults('string');
+  },
+  displayResults: function (typeFunction) {
+    console.log(typeFunction);
+    if (typeFunction == 'string') {
+      console.log(`Poll results: ${this.answers.join(',')}`);
+    } else if (typeFunction === 'array') {
+      console.log(this.answers);
+    }
+  },
 };
+
+const showPoll = poll.registerNewAnswer;
+
+document.querySelector('.poll').addEventListener('click', showPoll.bind(poll));
+
+const data1 = {
+  answers: [5, 2, 3],
+};
+
+const data2 = {
+  answers: [1, 5, 3, 9, 6, 1],
+};
+
+console.log(data1);
+
+const displayResultsNew = poll.displayResults;
+displayResultsNew.call(data1, 'string');
+displayResultsNew.call(data2, 'string');
+
+displayResultsNew.call(data1, 'array');
+displayResultsNew.call(data2, 'array');
